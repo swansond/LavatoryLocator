9d4ee833ca2e7da26e19c5bd9851b157a13adf1a
Chris Rovillos <crovillos@icloud.com>
2013-05-04 21:13:12 -0800

    Almost finished the LavatoryDetailActivity for the alpha release.

--- src/edu/washington/cs/lavatorylocator/LavatoryDetailActivity.java
+++ src/edu/washington/cs/lavatorylocator/LavatoryDetailActivity.java
@@ -4,29 +4,31 @@ import java.util.Arrays;
 import java.util.List;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
+/**
+ * Activity for viewing information about a specific lavatory.
+ * 
+ * @author Chris Rovillos
+ * 
+ */
 public class LavatoryDetailActivity extends ListActivity {
-    
+
     /**
      * Goes to the AddReviewActivity, giving it information about the bathroom
      * being displayed.
      * 
-     * @param item
-     *            the MenuItem that was clicked
+     * @param item the MenuItem that was clicked
      */
     public void addReview(MenuItem item) {
         // TODO
@@ -42,35 +44,40 @@ public class LavatoryDetailActivity extends ListActivity {
 
         // Intent intent = getIntent();
         // Bathroom bathroom = intent.getParcelableExtra(MainActivity.BATHROOM);
+
+        // TODO: for testing only; remove when networking code works
+        Bathroom bathroom = new Bathroom(1, 'M', "Mary Gates Hall", "3",
+                new Coordinates(1.0, 2.0), 5, 2.5);
+
+        List<Review> reviews = Arrays
+                .asList(new Review(0, 0, 0, 5, "lavatory 1"),
+                        new Review(1, 1, 26, 1, "Bad!"),
+                        new Review(2, 2, 97, 3, "OK."),
+                        new Review(3, 3, 48, 5, "Amazing!"),
+                        new Review(4, 4, 2, 4, "Good!"),
+                        new Review(5, 5, 106, 5,
+                                "Amazing! Amazing! Amazing! Amazing! Amazing! " +
+                                "Amazing! Amazing! Amazing! Amazing! Amazing! " +
+                                "Amazing! Amazing! Amazing! Amazing! Amazing! "));
         
-        //TODO: for testing only; remove when networking code works
-        Bathroom bathroom = new Bathroom(1, 'M', "Mary Gates Hall", "3", new Coordinates(1.0, 2.0), 5, 2.5);
+        getListView().setClickable(false); // TODO: remove when ReviewDetailActivity is implemented
         
-        List<Review> testReviews = Arrays.asList(new Review(0, 0, 0, 0, "lavatory 1"),
-                new Review(1, 1, 26, 1, "Bad!"),
-                new Review(2, 2, 97, 3, "OK."),
-                new Review(3, 3, 48, 5, "Amazing!"),
-                new Review(4, 4, 2, 4, "Good!"),
-                new Review(5, 5, 106, 5, "Amazing! Amazing! Amazing! Amazing! Amazing! Amazing! Amazing! Amazing! Amazing! Amazing! Amazing! Amazing!"));
+        setTitle("Lavatory " + bathroom.getBathroomID());
         
-        View headerView = getLayoutInflater().inflate(R.layout.activity_lavatory_detail_header, null);
-        ((TextView) headerView.findViewById(R.id.lavatory_detail_name_text)).setText("Lavatory " + bathroom.getBathroomID());
-        ((TextView) headerView.findViewById(R.id.lavatory_detail_building_text)).setText(bathroom.getBuilding());
-        ((RatingBar) headerView.findViewById(R.id.lavatory_detail_rating)).setRating((float) bathroom.getAverageRating());
-        getListView().addHeaderView(headerView);
-        
-        LavatoryDetailAdapter adapter = new LavatoryDetailAdapter(this, android.R.layout.simple_list_item_1, R.id.review_author, testReviews);
-        
+        View headerView = getLayoutInflater().inflate(
+                R.layout.activity_lavatory_detail_header, null);
+        ((TextView) headerView.findViewById(R.id.lavatory_detail_name_text))
+                .setText("Lavatory " + bathroom.getBathroomID());
+        ((TextView) headerView.findViewById(R.id.lavatory_detail_building_text))
+                .setText(bathroom.getBuilding());
+        ((RatingBar) headerView.findViewById(R.id.lavatory_detail_rating))
+                .setRating((float) bathroom.getAverageRating());
+        getListView().addHeaderView(headerView, null, false);
+
+        LavatoryDetailAdapter adapter = new LavatoryDetailAdapter(this,
+                R.layout.review_row, R.id.review_author, reviews);
+
         getListView().setAdapter(adapter);
-    }
-
-    /**
-     * Set up the {@link android.app.ActionBar}.
-     */
-    private void setupActionBar() {
-
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-
     }
 
     @Override
@@ -97,28 +104,47 @@ public class LavatoryDetailActivity extends ListActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Set up the {@link android.app.ActionBar}.
+     */
+    private void setupActionBar() {
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * Adapter for displaying an array of Review objects.
+     * 
+     * @author Chris Rovillos
+     * 
+     */
     private class LavatoryDetailAdapter extends ArrayAdapter<Review> {
 
         private List<Review> reviews;
-        
-        public LavatoryDetailAdapter(Context context, int resource, int textViewResourceId,
-                List<Review> objects) {
+
+        // TODO: comments
+        public LavatoryDetailAdapter(Context context, int resource,
+                int textViewResourceId, List<Review> objects) {
             super(context, resource, textViewResourceId, objects);
             this.reviews = objects;
         }
-        
+
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
-                convertView = getLayoutInflater().inflate(R.layout.review_row, parent, false);
+                convertView = getLayoutInflater().inflate(R.layout.review_row,
+                        parent, false);
             }
-            
-            ((RatingBar) convertView.findViewById(R.id.review_stars)).setRating(reviews.get(position).getRating());
-            ((TextView) convertView.findViewById(R.id.review_text)).setText(reviews.get(position).getReview());
-            
+
+            ((RatingBar) convertView.findViewById(R.id.review_stars))
+                    .setRating(reviews.get(position).getRating());
+            ((TextView) convertView.findViewById(R.id.review_author))
+                    .setText("User " + reviews.get(position).getUserID());
+            ((TextView) convertView.findViewById(R.id.review_text))
+                    .setText(reviews.get(position).getReview());
+
             return convertView;
         }
-        
+
     }
 
 }
