80b4e99c2792a06c6767aaf695841f35f274d009
crovillos <crovillos@icloud.com>
2013-05-05 23:11:46 -0700

    Made AddReviewActivity communicate with the server.

--- src/edu/washington/cs/lavatorylocator/AddReviewActivity.java
+++ src/edu/washington/cs/lavatorylocator/AddReviewActivity.java
@@ -1,18 +1,27 @@
 package edu.washington.cs.lavatorylocator;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.support.v4.app.NavUtils;
 
 /**
@@ -30,7 +39,15 @@ public class AddReviewActivity extends Activity {
      *            the <code>MenuItem</code> that was selected
      */
     public void addReview(MenuItem item) {
-        // TODO: implement
+        Intent intent = getIntent();
+        LavatoryData ld = intent.getParcelableExtra("LAVATORY");
+        
+        RatingBar ratingbar = ((RatingBar) findViewById(R.id.add_review_rating));
+        float rating = ratingbar.getRating();
+        EditText reviewText = ((EditText) findViewById(R.id.add_review_text));
+        String reviewTextString = reviewText.toString();
+        
+        updateReview("1", Integer.toString(ld.lavatoryID), Float.toString(rating), reviewTextString);
     }
 
     @Override
@@ -39,11 +56,6 @@ public class AddReviewActivity extends Activity {
         setContentView(R.layout.activity_add_review);
         // Show the Up button in the action bar.
         setupActionBar();
-        
-        //Example method call for testing
-        //TODO: move call to the right part of the activity and delete this
-        //updateReview("1", "2", "3", "4");
-
     }
 
     /**
@@ -117,4 +129,33 @@ public class AddReviewActivity extends Activity {
         new UpdateReviewTask().execute(hp);
     }
 
+    /**
+     * This class sends the data of a new review to the server.
+     * 
+     * @author Wil
+     *
+     */
+    private class UpdateReviewTask extends AsyncTask<HttpPost, Void, HttpResponse>{
+       
+        //The server communication that occurs in the background
+        @Override
+        protected HttpResponse doInBackground(HttpPost... hp) {
+            HttpClient client = new DefaultHttpClient();
+            try {
+                return client.execute(hp[0]);
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        
+        //executes when it finishes the server communication
+        protected void onPostExecute(HttpResponse hr) {
+            NavUtils.navigateUpFromSameTask(AddReviewActivity.this);
+        }
+    }
+    
 }
