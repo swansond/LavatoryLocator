c1b5bed0856ad073fcf8fe966f6e0e137ab2aa09
Chris Rovillos <crovillos@icloud.com>
2013-05-19 16:35:59 -0800

    Cleaned up request classes.

---
+++ src/edu/washington/cs/lavatorylocator/network/Got2goRequest.java
@@ -0,0 +1,43 @@
+package edu.washington.cs.lavatorylocator.network;
+
+import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceRequest;
+
+import edu.washington.cs.lavatorylocator.model.LavatoryData;
+
+/**
+ * Class for sending the Got2Go request to the LavatoryLocator service.
+ * 
+ * @author Chris Rovillos
+ * 
+ */
+public class Got2goRequest extends SpringAndroidSpiceRequest<LavatoryData> {
+
+    private static final String GOT2GO_SERVICE_URL = "http://lavlocdb.herokuapp.com/got2go.php";
+
+    private double latitude;
+    private double longitude;
+
+    /**
+     * Creates a new {@link Got2goRequest} with the given latitude and
+     * longitude.
+     * 
+     * @param latitude
+     *            the latitude
+     * @param longitude
+     *            the longitude
+     */
+    public Got2goRequest(double latitude, double longitude) {
+        super(LavatoryData.class);
+
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    @Override
+    public LavatoryData loadDataFromNetwork() throws Exception {
+        // TODO: use URL builder instead
+        String requestUrl = GOT2GO_SERVICE_URL + "?locationLat=" + latitude
+                + "&locationLong=" + longitude;
+        return getRestTemplate().getForObject(requestUrl, LavatoryData.class);
+    }
+}
--- src/edu/washington/cs/lavatorylocator/network/LavatorySearchRequest.java
+++ src/edu/washington/cs/lavatorylocator/network/LavatorySearchRequest.java
@@ -5,7 +5,7 @@ import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceReques
 import edu.washington.cs.lavatorylocator.model.LavatorySearchResults;
 
 /**
- * Class for sending lavatory search queries via the network.
+ * Class for sending search queries to the LavatoryLocator service.
  * 
  * @author Chris Rovillos
  * 
@@ -13,43 +13,56 @@ import edu.washington.cs.lavatorylocator.model.LavatorySearchResults;
 public class LavatorySearchRequest extends
         SpringAndroidSpiceRequest<LavatorySearchResults> {
     private static final String LAVATORY_SEARCH_SERVICE_URL = "http://lavlocdb.herokuapp.com/lavasearch.php";
-    
+
     private String building;
     private String floor;
     private String room;
+
     private double minRating;
     private char type;
+
     private String latitude;
     private String longitude;
     private String radius;
-    
+
     private boolean loadAllLavatories;
 
     /**
-     * Creates a new {@link LavatorySearchRequest}.
+     * Creates a new {@link LavatorySearchRequest} to request all lavatories.
      */
     public LavatorySearchRequest() {
         super(LavatorySearchResults.class);
-        
+
         loadAllLavatories = true;
     }
-    
+
     /**
+     * Creates a new {@link LavatorySearchRequest} to request lavatories that
+     * match the given parameters.
+     * 
      * @param building
+     *            the building the lavatory is in
      * @param floor
+     *            the floor the lavatory is on
      * @param room
+     *            the lavatory's room number
      * @param minRating
-     * @param maxDistance
+     *            the minimum rating of lavatories to be returned
      * @param type
+     *            the type of lavatory
      * @param latitude
+     *            the latitude of the search request point
      * @param longitude
+     *            the longitude of the search request point
      * @param radius
-     * @param loadAllLavatories
+     *            the radius of lavatories to return, as measured from the
+     *            search request point
      */
-    public LavatorySearchRequest(String building, String floor, String room, double minRating, char type, String latitude, String longitude,
+    public LavatorySearchRequest(String building, String floor, String room,
+            double minRating, char type, String latitude, String longitude,
             String radius) {
         super(LavatorySearchResults.class);
-        
+
         this.building = building;
         this.floor = floor;
         this.room = room;
@@ -58,6 +71,7 @@ public class LavatorySearchRequest extends
         this.latitude = latitude;
         this.longitude = longitude;
         this.radius = radius;
+
         this.loadAllLavatories = false;
     }
 
@@ -65,12 +79,16 @@ public class LavatorySearchRequest extends
     public LavatorySearchResults loadDataFromNetwork() throws Exception {
         if (loadAllLavatories) {
             return getRestTemplate().getForObject(LAVATORY_SEARCH_SERVICE_URL,
-                LavatorySearchResults.class);
+                    LavatorySearchResults.class);
         } else {
             // TODO: use URL builder instead
-            String requestUrl = LAVATORY_SEARCH_SERVICE_URL + "?locationLat=" + latitude + "&locationLong=" + longitude +
-                    "&maxDist=" + radius + "&bldgName=" + building + "&floor=" + floor + "&room=" + room + "&minRating=" + minRating + "&lavaType=" + type;
-            return getRestTemplate().getForObject(requestUrl, LavatorySearchResults.class);
+            String requestUrl = LAVATORY_SEARCH_SERVICE_URL + "?locationLat="
+                    + latitude + "&locationLong=" + longitude + "&maxDist="
+                    + radius + "&bldgName=" + building + "&floor=" + floor
+                    + "&room=" + room + "&minRating=" + minRating
+                    + "&lavaType=" + type;
+            return getRestTemplate().getForObject(requestUrl,
+                    LavatorySearchResults.class);
         }
     }
 }
