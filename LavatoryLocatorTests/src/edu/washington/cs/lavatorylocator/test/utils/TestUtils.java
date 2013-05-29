baed86a256dc588ab3a9ee194de2c5d92f7633e8
Chris Rovillos <crovillos@icloud.com>
2013-05-28 18:07:45 -0700

    Added first round of tests.

---
+++ LavatoryLocatorTests/src/edu/washington/cs/lavatorylocator/test/utils/TestUtils.java
@@ -0,0 +1,48 @@
+package edu.washington.cs.lavatorylocator.test.utils;
+
+import android.content.Context;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.jayway.android.robotium.solo.Solo;
+
+/**
+ * Utilities for testing.
+ * 
+ * @author Chris Rovillos
+ * 
+ */
+public class TestUtils {
+    // Prevent instantiation of the class
+    private TestUtils() {
+    }
+
+    /**
+     * Dismisses the Google Play Services error dialog if present.
+     * 
+     * @param solo
+     *            the {@link Solo} object used for testing
+     */
+    public static void dismissGooglePlayServicesErrorDialog(Solo solo) {
+        final Context applicationContext = solo.getCurrentActivity()
+                .getApplicationContext();
+        if (!googlePlayServicesAreAvailable(applicationContext)) {
+            solo.goBack();
+        }
+    }
+
+    /**
+     * Returns true if Google Play Services are available. Returns false
+     * otherwise.
+     * 
+     * @param context
+     *            the application context
+     * @return true if Google Play Services are available; false otherwise
+     */
+    private static boolean googlePlayServicesAreAvailable(Context context) {
+        final int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(context);
+
+        return (resultCode == ConnectionResult.SUCCESS);
+    }
+}
